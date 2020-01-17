5dbaad974856e3408ca54f69e8f282e0d9b69fb7
Nasra <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-01-17 09:19:09 -0800

    finished CRUD for actions

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -30,4 +30,57 @@ router.get("/:id", (req, res) => {
     })
 })
 
+router.post("/", (req, res) => {
+    const actionData = req.body;
+    if (!actionData.project_id || !actionData.description || !actionData.notes){
+        res.status(400).json({error: "Please provide a project id, description, and notes."})
+      } else {
+        Actions.insert(req.body)
+        .then(project => {
+          res.status(201).json(project)
+        })
+        .catch(err => {
+          console.log(err)
+          res.status(500).json({message: "There was an error while saving the action to the database."})
+        })
+    }
+})
+
+router.put("/:id", (req, res) => {
+    const actionData = req.body;
+    const id = req.params.id;
+    if (!actionData.project_id || !actionData.description || !actionData.notes){
+        res.status(400).json({error: "Please provide a project id, description, and notes."})
+      } else {
+        Actions.update(actionData, id)
+        .then(action => {
+            if (project) {
+                res.status(200).json(action)
+            } else {
+                res.status(404).json({ message: "The action with the specified ID doesn't exist."})
+            }
+        })
+        .catch(err => {
+          console.log(err)
+          res.status(500).json({message: "There was an error while saving the action to the database."})
+        })
+    }
+})
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    Actions.remove(id)
+    .then(action => {
+        if (action) {
+            res.status(200).json(action)
+        } else {
+            res.status(404).json({ message: "The action with the specified ID does not exist." })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: "The action could not be removed." })
+    })
+})
+
 module.exports = router;
